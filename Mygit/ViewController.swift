ef73745021d2cdfe57c3f96d3ebb977bfeaaf9de
ipBill <SnakeEyes.003@gmail.com>
2017-07-29 17:20:39 +0700

    Initial Commit

---
+++ Mygit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mygit
+//
+//  Created by Kornchawan Sawangjaeng on 7/29/2560 BE.
+//  Copyright © 2560 Kornchawan Sawangjaeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
